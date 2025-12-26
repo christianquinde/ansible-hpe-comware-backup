a9f6840cf6a1e09a19859c65e26d0e3a144e3840
Christian Quinde <christian.quinde@outlook.com>
2025-12-26 16:50:06 -0500

    v2: Use Ansible Vault for storing switch passwords

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ podman run --rm -it \
   -v $(pwd):/ansible:Z \
   -v $(pwd)/ssh_config:/root/.ssh/config:Z \
   ansible-comware \
-  ansible-playbook backup-comware.yml
+  ansible-playbook backup-comware.yml --ask-vault-password
